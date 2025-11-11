be4ab943469507a4f83686ca36679a57f220cded
Mohammed Baba <mohammedbaba1505@gmail.com>
2025-11-11 16:49:14 +0100

    manage state for score and index

--- react-quiz-app/src/pages/Quiz.jsx
+++ react-quiz-app/src/pages/Quiz.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import questions from "../data/questions"
+import { useState } from "react"
 
 const Quiz = () => {
+
+  const [index, setIndex] = useState(0);
+  const [score, setScore] = useState(0);
+
+  const current = questions[index];
   return (
     <div>Quiz</div>
   )
