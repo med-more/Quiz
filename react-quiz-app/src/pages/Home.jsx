e43be989b662562badbe59b53fc076ab39e40108
Mohammed Baba <mohammedbaba1505@gmail.com>
2025-11-12 15:11:18 +0100

    add the home page

--- react-quiz-app/src/pages/Home.jsx
+++ react-quiz-app/src/pages/Home.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
-
+import { Link } from "react-router-dom"
 const Home = () => {
   return (
-    <div>Home</div>
+    <div style={{textAlign: "center", marginTop: "40px"}}>
+      <h1>Quiz App</h1>
+      <p>Teste tes connaissances en développement web !</p>
+
+      <Link to="/quiz">
+        <button style={{padding: "10px 20px", marginTop: "20px"}}>
+          Commencer le quiz
+        </button>
+      </Link>
+    </div>
   )
 }
 
