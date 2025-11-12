a5b41609ca31af6fe78db85827e332c875f9400f
Mohammed Baba <mohammedbaba1505@gmail.com>
2025-11-12 12:35:12 +0100

    Add answer handling logic for quiz progression and scoring

--- react-quiz-app/src/pages/Quiz.jsx
+++ react-quiz-app/src/pages/Quiz.jsx
@@ -1,5 +1,7 @@
+import { useNavigate } from "react-router-dom";
 import questions from "../data/questions"
 import { useState } from "react"
+const navigate = useNavigate();
 
 const Quiz = () => {
 
@@ -7,6 +9,18 @@ const Quiz = () => {
   const [score, setScore] = useState(0);
 
   const current = questions[index];
+
+  function handleAnswer(option){
+    if (option === current.answer) {
+      setScore(score+1);
+    }
+
+    if (index + 1 < questions.length) {
+      setIndex(index + 1)
+    } else{
+      navigate("/result", {state : {score, total: questions.length}})
+    }
+  }
   return (
     <div>Quiz</div>
   )
