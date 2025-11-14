093be1f058297550b2385e99c90a4abab5ef7ac4
Mohammed Baba <mohammedbaba1505@gmail.com>
2025-11-14 10:46:07 +0100

    add the message based on the score

--- react-quiz-app/src/pages/Result.jsx
+++ react-quiz-app/src/pages/Result.jsx
@@ -6,10 +6,22 @@ const Result = () => {
 
   const score = location.state?.score || 0;
   const total = location.state?.total || 0;
+
+  let message = "";
+
+  if (score < 5) {
+    message = "Peut mieux faire"
+  } else if(score >=5 && score<=8) {
+    message = "Bravo"
+  } else if(score > 8){
+    message = "Excellent"
+  }
   return (
     <div style={{textAlign: "center", marginTop: "40px"}}>
       <h1>Result</h1>
       <h2>{score} / {total}</h2>
+
+      <h3 style={{marginTop: "20px"}}>{message}</h3>
 
       <button
         onClick={() => navigate("/quiz")}
